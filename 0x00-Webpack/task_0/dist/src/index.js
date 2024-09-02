77f2bb07d43c36dfe1843750c80139eb29a740e4
marwasabon <marwasabon@gmail.com>
2024-09-02 21:51:17 +0000

    task0

--- 0x00-Webpack/task_0/dist/src/index.js
+++
@@ -1,5 +0,0 @@
-import $ from 'jquery';
-//importing and appending
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
